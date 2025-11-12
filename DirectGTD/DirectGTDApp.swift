3c8e7c42a34a0b569fca6a3cfc5ac3aaf0e87bc6
Behrooz Karjoo <bkarjoo@gmail.com>
2025-11-11 23:08:17 -0500

    Initial Commit

---
+++ DirectGTD/DirectGTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirectGTDApp.swift
+//  DirectGTD
+//
+//  Created by Behrooz Karjoo on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DirectGTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
